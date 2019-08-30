88aa28d64b85893cb69564e25999690df2094fef
Marcos Lund <mlund@itba.edu.ar>
2019-08-30 15:48:55 -0300

    Update Python Ovito script

--- render.py
+++ render.py
@@ -1,27 +1,32 @@
 from ovito.io import import_file
-from ovito.vis import VectorDisplay
 from ovito.modifiers import *
 
-node = import_file("ovito_output.xyz", multiple_frames = True, columns = 
-	['Particle Identifier', 'Radius', 'Position.X', 'Position.Y', 'Velocity.X', 'Velocity.Y',
-	 'Vector Color.R', 'Vector Color.G', 'Vector Color.B'])
-# id:I:1:radius:R:1:pos:R:2:velo:R:2:color:R:3
-node.source.particle_properties.position.display.enabled = False
+node = import_file("C:\\Users\\Marcos\\git\\ss-2019-tp3\\ovito_output.xyz", multiple_frames = True, columns = ['Particle Identifier',
+    'Radius', 'Mass', 'Position.X', 'Position.Y', 'Velocity.X', 'Velocity.Y'])
+# id:I:1:radius:R:1:mass:R:1:pos:R:2:velo:R:2
 node.add_to_scene()
 
-modifier = CalculateDisplacementsModifier()
-modifier.reference.load("ovito_output.xyz", multiple_frames = True, columns = 
-	['Particle Identifier', 'Radius', 'Position.X', 'Position.Y', 'Velocity.X', 'Velocity.Y',
-	 'Vector Color.R', 'Vector Color.G', 'Vector Color.B'])
-modifier.affine_mapping = CalculateDisplacementsModifier.AffineMapping.ToCurrent
-modifier.frame_offset = 1
-modifier.use_frame_offset = True
-modifier.vector_display.enabled = True
-modifier.vector_display.reverse = True
-modifier.vector_display.shading = VectorDisplay.Shading.Flat
-modifier.vector_display.scaling = 25.0
-modifier.vector_display.width = 0.1
-modifier.vector_display.alignment = VectorDisplay.Alignment.Base
+# Periodic Boundary Condition (X,Y,Z)
+node.source.cell.pbc = (False, False, False)
+
+# Cell dimensions
+# cell_matrix = node.source.cell.matrix
+# cell_matrix.setflags(write = 1)
+# for i in range(len(cell_matrix)):
+#     cell_matrix[i][i] = 0.5
+# cell_matrix.setflags(write = 0)
+
+# Cell Transformation (resize)
+
+modifier = AffineTransformationModifier(
+    # operate_on = {'cell'}, # in Ovito 3
+    transform_particles = False, # in Ovito 2.9
+    transform_surface = False, # in Ovito 2.9
+    transform_vector_properties = False, # in Ovito 2.9
+    transform_box = True, # in Ovito 2.9
+    transformation = [[0.5, 0, 0, 0],
+                      [0, 0.5, 0, 0],
+                      [0, 0, 0.5, 0]])
 
 node.modifiers.append(modifier)
 node.compute()
