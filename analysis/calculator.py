e2f7e1ae82d3d749d765608157e1942768bba0e8
ramadis <rramiro.o@hotmail.com>
2019-08-31 22:28:11 -0300

    Add linear regression and diffusion calculations

--- analysis/calculator.py
+++ analysis/calculator.py
@@ -5,3 +5,13 @@ def calculateDistance(a, b):
   a = numpy.array(a)
   b = numpy.array(b)
   return numpy.linalg.norm(a-b)
+
+def squareList(lst):
+  return [x ** 2 for x in lst]
+
+def averageLists(lists):
+  return numpy.mean(numpy.array([ i for i in lists]), axis=0 )
+
+def linearRegression(data):
+  m,b = np.polyfit(range(len(data)), data, 1)
+  return [m, b]
