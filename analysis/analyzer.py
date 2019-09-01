a0d5910c238cb74d47f93e67816540092fb31356
ramadis <rramiro.o@hotmail.com>
2019-08-31 22:59:07 -0300

    Read directory from args. Extend diffusion function.

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -1,16 +1,22 @@
-from parser import parseFile
+from parser import parseDirectoryFromArgs
 from calculator import calculateDistance, squareList, averageLists, linearRegression
 
-def getDistancesFromOrigin(simulation):
+def getBallDistancesFromOrigin(simulation):
   firstBall = simulation.steps[0].ball
   distances = [calculateDistance(firstBall.position(), step.ball.position()) for step in simulation.steps]
   return distances
 
-def calculateDiffusion(simulations):
-  squaredDistances = [squareList(getDistancesFromOrigin(simulation)) for simulation in simulations]
+def getParticleDistancesFromOrigin(simulation, index = 5):
+  firstParticle = simulation.steps[0].particles[index]
+  distances = [calculateDistance(firstParticle.position(), step.particles[index].position()) for step in simulation.steps]
+  return distances
+
+def calculateDiffusion(simulations, getDistanceFromOrigin = getBallDistancesFromOrigin):
+  squaredDistances = [squareList(getDistanceFromOrigin(simulation)) for simulation in simulations]
   averageSquaredDistances = averageLists(squaredDistances)
-  [diffusion] = linearRegression(averageSquaredDistances)
+  print(squaredDistances)
+  diffusion = linearRegression(averageSquaredDistances)
   return diffusion
 
-simulation = parseFile('test.xyz')
-getDistancesFromOrigin(simulation)
+simulations = parseDirectoryFromArgs()
+print(calculateDiffusion(simulations))
