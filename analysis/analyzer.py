94f002df8538d2397a6c5c6d50e8318b6e2eb98c
ramadis <rramiro.o@hotmail.com>
2019-08-31 22:28:11 -0300

    Add linear regression and diffusion calculations

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -1,10 +1,16 @@
 from parser import parseFile
-from calculator import calculateDistance
+from calculator import calculateDistance, squareList, averageLists, linearRegression
 
 def getDistancesFromOrigin(simulation):
   firstBall = simulation.steps[0].ball
   distances = [calculateDistance(firstBall.position(), step.ball.position()) for step in simulation.steps]
   return distances
+
+def calculateDiffusion(simulations):
+  squaredDistances = [squareList(getDistancesFromOrigin(simulation)) for simulation in simulations]
+  averageSquaredDistances = averageLists(squaredDistances)
+  [diffusion] = linearRegression(averageSquaredDistances)
+  return diffusion
 
 simulation = parseFile('test.xyz')
 getDistancesFromOrigin(simulation)
